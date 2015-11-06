d6ac619b5b0103bfa6a129b9255b65e87a27d106
Patrik Rydberg <rydan@cutpaste.org>
2015-11-06 11:32:25 +0100

    Tests updated to test List.get() out of bounds

--- test/org/cutpaste/utils/NoNullTest.java
+++ test/org/cutpaste/utils/NoNullTest.java
@@ -1,7 +1,9 @@
 package org.cutpaste.utils;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static junit.framework.TestCase.assertEquals;
@@ -32,6 +34,27 @@ public class NoNullTest {
         //Fetched the value
         assertTrue(value4.isPresent());
         assertEquals("TestString", value4.get());
+    }
+
+    @Test
+    public void testIncludingList() {
+        ArrayList<TestA> list = new ArrayList<>();
+        Optional<String> s =  NoNull.get(() -> list.get(0).getTestString());
+        //Fetched from empty list
+        assertFalse(s.isPresent());
+
+        TestA obj = new TestA();
+        String testString = "Hepp!";
+        obj.setTestString(testString);
+        list.add(obj);
+        s =  NoNull.get(() -> list.get(0).getTestString());
+        //Fetched correct object
+        assertTrue(s.isPresent());
+        assertEquals(testString, s.get());
+
+        s = NoNull.get(() -> list.get(1).getTestString());
+        //Fetch out of list bounds
+        assertFalse(s.isPresent());
     }
 
     private static final class TestA {
