f2140006cbd41a007a64be4cba7acedadb0f7b30
Patrik Rydberg <rydan@cutpaste.org>
2015-11-06 11:31:37 +0100

    NoNull now also handle List.get() out of bounds

--- src/org/cutpaste/utils/NoNull.java
+++ src/org/cutpaste/utils/NoNull.java
@@ -8,12 +8,13 @@ public class NoNull {
     /**
      *
      * get uses an ExecuteAroundMethod pattern to remove the need for null checks during a get-like operation
+     * (It will also handle List.get() outside list bounds)
      *
      * get is meant to encapsulate null checks when doing chained gets, where any part in the chain might return null.
      * (But it can be used in pretty much any situation where a single value needs to be fetched, and steps along the way might
-     * result in null pointer exceptions)
+     * result in null pointer exceptions or IndexOutOfBoundsException)
      *
-     * The Optional will be empty if the function completes successfully but returns null, or if there is a null pointer
+     * The Optional will be empty if the function completes successfully but returns null, or if there is a null pointer/index out of bounds
      * exception thrown at any time during the execution of the supplied function
      *
      * @param function to execute for value
@@ -24,7 +25,7 @@ public class NoNull {
     public static <T> Optional<T> get(Supplier<T> function) {
         try {
             return Optional.ofNullable(function.get());
-        } catch (NullPointerException e) {
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
             //OK, just return empty Optional
         }
         return Optional.empty();
